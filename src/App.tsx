64a1e457f0e906d75b91432bac158e321423b9bb
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-11-19 09:16:35 +0200

    importing social to app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,10 @@
-
+import "./App.css";
+import Social from "./social-link/social";
+function App() {
+  return (
+    <div className="app">
+      <Social />  
+    </div>
+  );
+}
+export default App;
